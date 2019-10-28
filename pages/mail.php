17070b9c39afa59f2573a556304cf9c4f64d8bfd
Denis Romanenko <romadenn@gmail.com>
2019-10-28 16:07:45 +0300

    add validate input type=file

--- pages/mail.php
+++ pages/mail.php
@@ -14,7 +14,7 @@ $textsend = $_POST['message'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'stokelshop@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'stokelshop1@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'wsrfyh55'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
--- scripts/form.js
+++ scripts/form.js
@@ -2,9 +2,21 @@ class ValidForm {
   validInput(name, error) {
     if (name.checkValidity()) {
       error.style.visibility = "hidden";
-      console.log("all is ok");
     } else {
       error.style.visibility = "visible";
+    }
+  }
+  validFile(form, button, error) {
+    const size = 10485760; // bytes
+    let file = form.elements["upload"].files[0];
+    if (file.size > size) {
+      button.disabled = true;
+      button.classList.add("btnDisabled");
+      error.style.visibility = "visible";
+    } else {
+      button.disabled = false;
+      button.classList.remove("btnDisabled");
+      error.style.visibility = "hidden";
     }
   }
 }
@@ -12,6 +24,8 @@ class ValidForm {
 const form = document.forms.order;
 const errorName = form.querySelector(".error-name");
 const errorPhone = form.querySelector(".error-phone");
+const errorFile = form.querySelector(".error-file");
+const button = form.querySelector(".btn-12");
 
 const validForm = new ValidForm();
 
@@ -21,4 +35,5 @@ form.addEventListener("input", event => {
   const phone = form.elements.user_phone;
   validForm.validInput(name, errorName);
   validForm.validInput(phone, errorPhone);
+  validForm.validFile(form, button, errorFile);
 });
